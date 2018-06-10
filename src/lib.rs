f41e319be3d7d0202de8c94f70587969b9d95985
zhenren <wangweizhen@bilibili.com>
2018-06-10 19:02:34 +0800

    refactor

--- src/lib.rs
+++ src/lib.rs
@@ -26,34 +26,6 @@ macro_rules! rep_no_copy {
     }};
 }
 
-/// Panics if the pointer is not properly unaligned.
-#[inline]
-fn ensure_aligned<T>(raw: *const T) {
-    assert_eq!(raw as usize & low_bits::<T>(), 0, "unaligned pointer");
-}
-
-/// Returns a bitmask containing the unused least significant bits of an aligned pointer to `T`.
-#[inline]
-fn low_bits<T>() -> usize {
-    (1 << mem::align_of::<T>().trailing_zeros()) - 1
-}
-
-/// Given a tagged pointer `data`, returns the same pointer, but tagged with `tag`.
-///
-/// `tag` is truncated to fit into the unused bits of the pointer to `T`.
-#[inline]
-fn data_with_tag<T>(data: usize, tag: usize) -> usize {
-    (data & !low_bits::<T>()) | (tag & low_bits::<T>())
-}
-
-/// Decomposes a tagged pointer `data` into the pointer and the tag.
-#[inline]
-fn decompose_data<T>(data: usize) -> (*mut T, usize) {
-    let raw = (data & !low_bits::<T>()) as *mut T;
-    let tag = data & low_bits::<T>();
-    (raw, tag)
-}
-
 enum NodeType {
     NODE4,
     NODE16,
@@ -90,19 +62,17 @@ pub struct NodeHeader {
 }
 
 pub fn is_locked(version: &Arc<AtomicUsize>) -> bool {
-    let (_, tag) = decompose_data::<u64>(version.load(Ordering::SeqCst));
-    tag & 0b10 == 0b10
+    version.load(Ordering::SeqCst) & 0b10 == 0b10
 }
 
 pub fn is_obsolete(version: &Arc<AtomicUsize>) -> bool {
-    let (_, tag) = decompose_data::<u64>(version.load(Ordering::SeqCst));
-    tag & 1 == 1
+    version.load(Ordering::SeqCst) & 1 == 1
 }
 
 impl NodeHeader {
     pub fn new() -> Self {
         NodeHeader {
-            version: Arc::new(AtomicUsize::new(data_with_tag::<u64>(0, 0))),
+            version: Arc::new(AtomicUsize::new(0)),
             num_children: 0,
             partial_len: 0,
             partial: unsafe { mem::uninitialized() },
@@ -168,8 +138,7 @@ impl NodeHeader {
 
     fn write_unlock(&self) {
         let ver = self.version.load(Ordering::SeqCst);
-        self.version
-            .store(data_with_tag::<u64>(ver, 0b0), Ordering::SeqCst);
+        self.version.store(ver + 3, Ordering::SeqCst);
     }
 }
 
