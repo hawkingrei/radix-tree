55fd847134a63c6d274f359e739a768fb4464283
真人 <weizhen.wang@duitang.com>
2018-06-17 21:57:14 +0800

    remove unused

--- src/lib.rs
+++ src/lib.rs
@@ -15,9 +15,3 @@ use crossbeam_epoch::{pin, unprotected, Atomic, Guard, Owned, Shared};
 
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::Arc;
-
-const RADIX_TREE_MAP_SHIFT: usize = 6;
-const EMPTY_CELL: u8 = 0;
-
-pub const SMALL_STRUCT: usize = 8;
-type Small = [u8; SMALL_STRUCT];
